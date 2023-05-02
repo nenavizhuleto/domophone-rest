c68b853d9db73eb22c552a508e31527647a1b3d1
nl <nenavizhu.leto@gmail.com>
2023-05-02 16:34:58 +0500

    authenticatetoken rework and understanding

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,20 +1,15 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import jwt from 'jsonwebtoken';
 
-export const authenticateToken = (req: Request, res: Response, next) => {
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
 	const authHeader = req.headers['authorization'];
 	const token = authHeader && authHeader.split(' ')[1];
 
 	if (token == null) return res.sendStatus(401);
 
-	jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, payload: any) => {
-		console.log(err);
-
+	jwt.verify(token, process.env.JWT_SECRET as string, (err: any, payload: jwt.JwtPayload) => {
 		if (err) return res.sendStatus(403);
-		Object.defineProperty(req, 'user', {
-			value: payload,
-		});
 
 		next();
 	});
